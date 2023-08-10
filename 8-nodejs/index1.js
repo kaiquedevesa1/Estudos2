90ecd892d3ffd4fd2ec0fdbb0a421f3b0e2087a6
Tochyex <kaiquedeveza@hotmail.com>
2023-08-10 19:26:05 -0300

    criando rotas

--- 8-nodejs/index1.js
+++ 8-nodejs/index1.js
@@ -15,9 +15,16 @@ app.get("/usuario/:nome", (req,resp)=>{
     resp.send("<h1>Exemplos de Rota</h1><p style= 'color: #00FF00' >Valor do parâmetro passado: "+ req.params.nome + "</p>")
 })
 
+app.get("/user2", (req,resp)=>{
+    resp.send("<h1>Envio de usuário por get</h1>  <a href= '/view-usuario?nomeUsuario=Mauro'>VIEW</a>")
+})
 
-
-
+app.get("/view-usuario", (req,resp)=>{
+    
+    let nmUser= req.query.nomeUsuario
+    
+    resp.send(`<h1> Parametro recebido através de envio por GET</h1><p>Usuário : ${nmUser} </p>`)
+})
 
 
 app.listen(3000,()=>{
