c0fff220284dee1c6dba9bbeb470405e3a7e8280
Tochyex <kaiquedeveza@hotmail.com>
2023-08-10 20:21:11 -0300

    atulizando os arquivos

--- 8-nodejs/index1.js
+++ 8-nodejs/index1.js
@@ -1,23 +1,40 @@
 import express from "express"
+import path from "path" //
+import { fileURLToPath } from "url" //Ajuda a criar caminhos dinâmicos no projeto
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))// fileURL faz uma indiçacao para pastas indeternas dentro das estruturas, que irao ser criados scripts que serao manipulados pelo node.js apos passarmos os parametros
+
 
 const app = express()
 
-app.get("/", (req,res)=>{
-    res.send("<p>Servidor Criado!</p>")
+app.get("/", (req, res )=>{
+    res.sendFile("public/index.html",  {root : __dirname})           //Ira ser feito um envio de um arquivo inteiro atraves de um diretorio nao apenas de um texto
+
 })
+
+
+//**app.get("/", (req,res)=>{
+    //res.send("<p>Servidor Criado!</p>")//
+//})**//
 
 
 app.get("/NovaRota",(req,res)=>{
     res.send("<h1 style= 'color: #0000FF'>Nova rota criada com sucesso</h1>")
 })
 
+
+
 app.get("/usuario/:nome", (req,resp)=>{
     resp.send("<h1>Exemplos de Rota</h1><p style= 'color: #00FF00' >Valor do parâmetro passado: "+ req.params.nome + "</p>")
 })
 
+
+
 app.get("/user2", (req,resp)=>{
     resp.send("<h1>Envio de usuário por get</h1>  <a href= '/view-usuario?nomeUsuario=Mauro'>VIEW</a>")
 })
+
+
 
 app.get("/view-usuario", (req,resp)=>{
     
